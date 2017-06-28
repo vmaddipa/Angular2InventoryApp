3c9e38c92b2497dd4c93777f134a76ebbf1539ae
Vamsi M <vamsi.maddipatla@a2-g.com>
2017-06-28 11:45:55 -0400

    Added Products array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,31 @@ import {Product} from "./app";
   styleUrls: ['./app.component.css']
 })
 export class InventoryApp {
-  product: Product;
+  products: Product[];
 
 constructor() {
-this.product = new Product(
-'NICEHAT', 'A Nice Black Hat',
- '/resources/images/products/black-hat.jpg',
+this.products = [
+ new Product(
+ 'MYSHOES',
+'Black Running Shoes',
+ '/resources/images/products/black-shoes.jpg',
+['Men', 'Shoes', 'Running Shoes'],
+ 109.99),
+ new Product(
+ 'NEATOJACKET',
+ 'Blue Jacket',
+'/resources/images/products/blue-jacket.jpg',
+ ['Women', 'Apparel', 'Jackets & Vests'],
+ 238.99),
+new Product(
+ 'NICEHAT',
+'A Nice Black Hat',
+'/resources/images/products/black-hat.jpg',
+
 ['Men', 'Accessories', 'Hats'],
- 29.99);
-}
+29.99)
+];
+ }
 title = 'Inventory App';
 }
   
